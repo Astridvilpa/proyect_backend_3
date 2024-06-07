317809c1843caa43e5deb0dfd57a79e65106abfd
Astrid Vilora <astridviloria54@gmail.com>
2024-06-07 12:03:40 +0200

    feat: Add middleware authorize

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const crtl = require("../controllers/userController");
 const auth = require("../middlewares/auth");
+const authorize = require("../middlewares/authorize");
 
 // User routes
 
@@ -11,11 +12,11 @@ router.put("/profile", auth, crtl.updateUserProfile);
 
 //protected routes
 
-router.get("/", crtl.getAll);
-router.get("/email", crtl.getUserByEmail);
-router.get("/:id", crtl.getById);
-router.put("/:id", crtl.update);
-router.delete("/:id", crtl.delete);
-router.get("/:id/appointments", crtl.getAppointmentsByUserId);
+router.get("/", auth, authorize("super_admin"), crtl.getAll);
+router.get("/email", auth, crtl.getUserByEmail);
+router.get("/:id", auth, crtl.getById);
+router.put("/:id", auth, crtl.update);
+router.delete("/:id", auth, crtl.delete);
+router.get("/:id/appointments", auth, crtl.getAppointmentsByUserId);
 
 module.exports = router;
